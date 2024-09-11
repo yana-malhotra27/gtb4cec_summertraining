1e39bc9ed546aaa1aebbeb94fec021743b0c8579
yana-malhotra27 <yana27.ym@gmail.com>
2024-09-11 15:12:49 +0530

    first

--- Week8/Main.java
+++ Week8/Main.java
@@ -3,18 +3,23 @@ package Week8;
  class Ash {
     int d=12;
     Ash(int d){
+        System.out.println("this is call ash");
         this.d=d;
     }
 }
 class Bash extends Ash { //error if parent as constructor then child class should call it
     int a=12;
-    Bash(int d){
-        super(d); //reference variable
+    Bash(int d,int a){
+        //this.a=a; //error if called before super
+        //super(a); //can only be once so error
+        super(d); //reference variable always call first
+        System.out.println("bash is called");
+        //this.a=a;
     }
 }
 public class Main{
     public static void main(String[] args) {
-        Bash obj=new Bash(30);
+        Bash obj=new Bash(30,40);
         System.out.println(obj.d);
     }
 }
