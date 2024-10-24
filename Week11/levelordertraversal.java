d43408cb45ca6ea878aa5eea507a8dbf77617f22
yana-malhotra27 <yana27.ym@gmail.com>
2024-10-24 13:27:50 +0530

    first

--- Week11/levelordertraversal.java
+++ Week11/levelordertraversal.java
@@ -3,8 +3,9 @@ package Week11;
 import java.util.Queue;
 //leetcode 101 mostly recurssion use in solving trees questions
 //951
-//hw-112 path sum, 129 sum of two leaves
+//hw-112 path sum, 129 sum of two leaves,98 validate
 //701 vimp
+//450 delete a node in bst
 //red black tree and avl tree
 public class levelordertraversal {
     //left before thu 24 oct 2024 20 min oops
