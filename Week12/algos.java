30fcd672b36789e9a08f6d90ff584c473ac714f8
yana-malhotra27 <yana27.ym@gmail.com>
2024-11-01 20:29:09 +0530

    first

---
+++ Week12/algos.java
@@ -0,0 +1,16 @@
+public class algos {//dp-memorisation and tabulation
+    //on graphs for operations
+    //prims -mst
+    //dijkstra -min cost and path
+    //bellman ford's algo -negative edge present path
+    //prims-min spanning tree remove cycles all nodes are covered path that covers less cost
+    //prims-priority queue
+    //priority que more used in real world 1 close to death 5 still in danger 9 almost safe ascending or descending
+    //dijkstra algo -sssp single source shortest path min cost path
+    //problem in dijkstara so introduced bellman ford- ex in directed graph negative path
+    //infinite loop problem when negative elements come in graph
+    //-ve path then travel again negative so values get reduced so bellman ford came
+    //he said if there's v vertex paths negative edges so run till v times
+    //prims short sighted it sees children so doesn't see negative part
+    //bellman ford lil tricky- v vertex in arraylist form till n vertices done
+}
